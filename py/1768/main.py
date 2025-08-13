d37fcc6d863f0a1ee13fc4d5c4d506adb9f714bc
Samuel Lee <leesamuel423@gmail.com>
2025-08-13 16:51:29 -0400

    [1768] optimize

--- py/1768/main.py
+++ py/1768/main.py
@@ -2,17 +2,15 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         output = ""
         w1 = 0
-        w2 = 0
 
-        while w1 < len(word1) and w2 < len(word2):
+        while w1 < len(word1) and w1 < len(word2):
             output += word1[w1]
-            output += word2[w2]
+            output += word2[w1]
             w1 += 1
-            w2 += 1
 
         if w1 < len(word1):
             output += word1[w1:]
-        elif w2 < len(word2):
-            output += word2[w2:]
+        elif w1 < len(word2):
+            output += word2[w1:]
 
         return output
